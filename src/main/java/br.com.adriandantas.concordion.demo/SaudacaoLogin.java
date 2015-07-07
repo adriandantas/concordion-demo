98ad591328ec0215a976e1a10f4fdb71a8ea0b2c
Adrián Dantas <adrian.dantas@gmail.com>
2015-07-07 14:36:42 -0300

    Classe responsável pela geração de mensagens de saudação.

---
+++ src/main/java/br.com.adriandantas.concordion.demo/SaudacaoLogin.java
@@ -0,0 +1,16 @@
+package br.com.adriandantas.concordion.demo;
+
+/**
+ * Gera mensagens de saudação para usuários.
+ */
+public class SaudacaoLogin {
+
+    /**
+     * Retorna mensagem de saudação.
+     *
+     * @return Saudação
+     */
+    public String getSaudacao() {
+        return "Bem-vindo";
+    }
+}
